bb528e6a43f8078cf547593995480226c07a06bc
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-14 15:21:31 -0300

    feat: teste unitario de insercao de 2 comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { fireEvent, getByTestId, render, screen } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve inserir dois comentários corretamente', () => {
+        const { debug } = render(<PostComment/>)
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'comentario 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-submit-comentario'))
+        fireEvent.change(screen.getByTestId('textarea-comentario'), {
+            target: {
+                value: 'comentario 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-submit-comentario'))
+
+        expect(screen.getAllByTestId('post-comentario')).toHaveLength(2)
+        debug()
+    })
 });
